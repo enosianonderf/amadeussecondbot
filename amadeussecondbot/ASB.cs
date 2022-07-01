e77bad001be9a11e8dab08fc65e343f8f4ef05e2
enosianonderf <106455825+enosianonderf@users.noreply.github.com>
2022-07-01 13:05:33 +0400

    Более лучшая версия запуска бота.

--- amadeussecondbot/ASB.cs
+++ amadeussecondbot/ASB.cs
@@ -1,63 +1,103 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Args;
+using System.Net;
 
 namespace amadeus2
 {
     class ASB
 // Токен и определение самого бота
-    {
+    {   
+
         public static int emo = 50;
         private static string token { get; set; } = "2008109914:AAFi-o9eFEgMaLO8k22F9Tmq4BmzwiROkLI";
         private static TelegramBotClient bot;
         static void Main(string[] args)
         {
+            
             RandomAngry a = new RandomAngry();
             RandomHappy h = new RandomHappy();
+
+            DateTime now = DateTime.Now;
             Console.WriteLine("Запуск бота...");
+            Console.WriteLine($"Инициализация бота {now:T}");
+            WebClient Client = new WebClient();
+            String Response;
+            try
+            {   
+                Response = Client.DownloadString("http://test.vkgroup.az/");
+                if (Response != null)
+                {   
+
+                    Console.WriteLine("Запуск успешен! Бот находится в исправно-активном состоянии.");
+                    Console.WriteLine($"Бот запущен {now:R}");
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("Запуск бота провален.");
+                Console.WriteLine($"Бот не работает. Попробуйте подключиться позже. {now:R}");
+            }
             
+
             bot = new TelegramBotClient(token);
             bot.OnMessage += BotOnOnMessage;
             bot.StartReceiving();
             Console.ReadLine();
             
         }
+        
+        
+
         private static async void BotOnOnMessage(object? sender, MessageEventArgs e)
         {
-            
-            RandomAngry a = new RandomAngry();
-            RandomHappy h = new RandomHappy();
+            using var stream = new MemoryStream();
+            RandomAngry a = new();
+            RandomHappy h = new();
             var msg = e.Message;
             var lower = msg.Text.ToLower();
+           
+
             if (msg.Text != null)
             {
+                /*if (emo <= 20)
+                {
+                    string emostatus = a.AngryRandomization();
+                }
+                else if (emo >= 85 && emo <= 100)
+                {
+                    string emostatus = h.HappyRandomization();
+                }
+                */
                 Console.WriteLine($"Введено : {msg.Text}");
-                if(lower.Contains("кристин"))
+                if(lower.Contains("кристи"))
                 {
                     await bot.SendTextMessageAsync(msg.Chat.Id, "Не называй меня Кристиной!");
                 }              
-                if (lower.Contains("привет"))
+                if (lower.Contains("прив"))
                 {
-                    await bot.SendPhotoAsync(
-                    chatId: msg.Chat.Id,
-                    parseMode: Telegram.Bot.Types.Enums.ParseMode.Html,
-                    photo: a.AngryRandomization(),
-                    caption: "Здравствуйте.");
+                    stream.Position = 0;
+                    emo = 30;
+                    await bot.SendPhotoAsync(chatId: msg.Chat.Id,
+                        parseMode: Telegram.Bot.Types.Enums.ParseMode.Html,
+                        photo: a.AngryRandomization(),
+                        caption: "Здравствуйте.");
                 }
                 if (lower.Contains("дура"))
                 {
-                    emo = emo - 20;
-                    await bot.SendPhotoAsync(
-                    chatId: msg.Chat.Id,
-                    photo: "https://github.com/enosianonderf/amadeussecondbot/blob/master/amadeussecondbot/amadeusemo/KurisuAngry/CRS_JLE_40000400.png?raw=true",
-                    caption: "Сам дурак!");
+                    stream.Position = 0;
+                    emo = 20;
+                    await bot.SendPhotoAsync(chatId: msg.Chat.Id,
+                        photo: a.AngryRandomization(),
+                        caption: "Сам дурак!");
                 }
                 if (lower.Contains("смеш"))
                 {
-                    emo = emo + 20;
-                    await bot.SendPhotoAsync(
-                    chatId: msg.Chat.Id,
-                    photo: h.HappyRandomization(),
-                    caption: "Хаха!");
+                    stream.Position = 0;
+                    emo = 85;
+                    await bot.SendPhotoAsync(chatId: msg.Chat.Id,
+                        photo: h.HappyRandomization(),
+                        caption: "Хаха!");
                 }
             }        
         }      
--- amadeussecondbot/RandomHappy.cs
+++ amadeussecondbot/RandomHappy.cs
@@ -10,10 +10,10 @@ namespace amadeus2
     {
         Random h = new Random();
 
-        string[] HappyEmos = new string[] { "https://raw.githubusercontent.com/enosianonderf/amadeussecondbot/master/amadeussecondbot/amadeusemo/KurisuFun/fun1.png?token=GHSAT0AAAAAABWEESIMKZBAK5YVCXD7OXKYYV5XORA",
-            "https://raw.githubusercontent.com/enosianonderf/amadeussecondbot/master/amadeussecondbot/amadeusemo/KurisuFun/fun2.png?token=GHSAT0AAAAAABWEESINBLVY4FRK6Q5PLDGUYV5XPIQ",
-            "https://raw.githubusercontent.com/enosianonderf/amadeussecondbot/master/amadeussecondbot/amadeusemo/KurisuFun/fun3.png?token=GHSAT0AAAAAABWEESINBUI4C56MZGVGQLOWYV5XPTA",
-            "https://raw.githubusercontent.com/enosianonderf/amadeussecondbot/master/amadeussecondbot/amadeusemo/KurisuFun/fun4.png?token=GHSAT0AAAAAABWEESIMDVEJ3VYGP5TBJYW4YV5XQNQ",
+        string[] HappyEmos = new string[] { "https://raw.githubusercontent.com/enosianonderf/amadeussecondbot/master/amadeussecondbot/amadeusemo/KurisuFun/fun1.png",
+            "https://raw.githubusercontent.com/enosianonderf/amadeussecondbot/master/amadeussecondbot/amadeusemo/KurisuFun/fun2.png?token=GHSAT0AAAAAABWEESIMUBHMVK3N7GOBB2RWYV5XQ4Q",
+            "https://raw.githubusercontent.com/enosianonderf/amadeussecondbot/master/amadeussecondbot/amadeusemo/KurisuFun/fun3.png?token=GHSAT0AAAAAABWEESIMUBHMVK3N7GOBB2RWYV5XQ4Q",
+            "https://raw.githubusercontent.com/enosianonderf/amadeussecondbot/master/amadeussecondbot/amadeusemo/KurisuFun/fun4.png?token=GHSAT0AAAAAABWEESIMUBHMVK3N7GOBB2RWYV5XQ4Q",
             "https://raw.githubusercontent.com/enosianonderf/amadeussecondbot/master/amadeussecondbot/amadeusemo/KurisuFun/fun5.png?token=GHSAT0AAAAAABWEESIMUBHMVK3N7GOBB2RWYV5XQ4Q",
             "https://raw.githubusercontent.com/enosianonderf/amadeussecondbot/master/amadeussecondbot/amadeusemo/KurisuFun/fun6.png?token=GHSAT0AAAAAABWEESIM6LOKGR5GHHMVIPVWYV5XRJA",
         };
@@ -21,8 +21,8 @@ namespace amadeus2
         public string HappyRandomization()
         {
             int randomNum = h.Next(0, 2);
-            string str = HappyEmos[randomNum];
-            return str;
+            string hapres = HappyEmos[randomNum];
+            return hapres;
         }
     }
 }
